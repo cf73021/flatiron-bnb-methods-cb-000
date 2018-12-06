83499b92e49d817972aa04937592ba69a9784927
Chris Franklin <cf73021@gmail.com>
2018-12-06 22:20:29 +0000

    Automatically backed up by Learn

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,14 @@ class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  validate :reserving_own_listing
 
+  def reserving_own_listing
+    return unless errors.blank?
+    if guest == listing.host
+      errors[:guest] = 'cannot be same as host'
+    end
+  end
 end
